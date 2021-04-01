2b0efbe0b8f9238b2df9bc771b651454ebb36272
Andrey <ansh.msk@gmail.com>
2021-04-01 21:12:28 +0300

    let's begin

---
+++ src/main/java/ru/kamuzta/beatboxrestfulserver/BeatboxrestfulserverApplication.java
@@ -0,0 +1,13 @@
+package ru.kamuzta.beatboxrestfulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatboxrestfulserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeatboxrestfulserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kamuzta/beatboxrestfulserver/BeatboxrestfulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kamuzta.beatboxrestfulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeatboxrestfulserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
