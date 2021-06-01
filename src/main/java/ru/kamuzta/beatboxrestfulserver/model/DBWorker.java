cf97449137809c543dace1c676142ba20b43fe2c
Andrey <ansh.msk@gmail.com>
2021-06-01 15:06:33 +0300

    mysql connection url added to sysenv JDBC_DATABASE_URL to enable heroku support

--- src/main/java/ru/kamuzta/beatboxrestfulserver/model/DBWorker.java
+++ src/main/java/ru/kamuzta/beatboxrestfulserver/model/DBWorker.java
@@ -1,19 +1,19 @@
 package ru.kamuzta.beatboxrestfulserver.model;
 
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBWorker {
-    private static final String URL = "jdbc:mysql://localhost:3306/kamuzta?useUnicode=true&serverTimezone=UTC&useSSL=false";
-    private static final String USERNAME = "kamuzta";
-    private static final String PASSWORD = "kamuzta";
 
     private Connection connection;
 
     public DBWorker() {
         try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            String dbUrl = System.getenv("JDBC_DATABASE_URL");
+            connection = DriverManager.getConnection(dbUrl);
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
